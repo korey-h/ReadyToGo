4c10d2e7900b71e3a4b52cb9571121eb263a3dd0
Егор Уфаев <eufaev@yandex.ru>
2022-03-16 00:05:35 +0500

    add utilities.py

--- ReadyToGo/registration/models.py
+++ ReadyToGo/registration/models.py
@@ -1,8 +1,9 @@
-from pickle import TRUE
 from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models import Q
+
+from .utilities import NumbersChecker
 
 START_NUM = 1
 
@@ -13,7 +14,7 @@ class Cups(models.Model):
         max_length=200,
     )
     slug = models.SlugField(max_length=70)
-    description = models.TextField(max_length=150, blank=True, null=TRUE,)
+    description = models.TextField(max_length=150, blank=True, null=True,)
 
     def __str__(self):
         return self.name
@@ -29,13 +30,13 @@ class Races(models.Model):
     cup = models.ForeignKey(Cups,
                             on_delete=models.CASCADE,
                             related_name='cup_races',
-                            blank=True, null=TRUE,)
+                            blank=True, null=True,)
     town = models.TextField(max_length=50)
     numbers_amount = models.IntegerField(
         validators=[MinValueValidator(1, message='Количество не меньше 1')
                     ]
     )
-    description = models.TextField(max_length=150, blank=True,  null=TRUE)
+    description = models.TextField(max_length=150, blank=True, null=True)
 
     def __str__(self):
         return self.name
@@ -59,10 +60,10 @@ class Categories(models.Model):
     year_yang = models.IntegerField(
         validators=[MinValueValidator(1940, message='Не старше 1940 г.р.'), ])
     number_start = models.IntegerField(
-        blank=True, null=TRUE,
+        blank=True, null=True,
         validators=[MinValueValidator(1, message='Значение не меньше 1'), ])
     number_end = models.IntegerField(
-        blank=True, null=TRUE,
+        blank=True, null=True,
         validators=[MinValueValidator(1, message='Значение не меньше 1'), ])
     description = models.TextField(max_length=150, blank=True)
 
@@ -88,7 +89,8 @@ class Participants(models.Model):
     patronymic = models.CharField(
         verbose_name="Отчество",
         max_length=30,
-        blank=True, null=TRUE,)
+        blank=True,
+        default='-',)
     year = models.IntegerField(
         validators=[MinValueValidator(1940, message='Не старше 1940 г.р.'), ],
         verbose_name="Год рождения")
@@ -97,71 +99,17 @@ class Participants(models.Model):
         verbose_name="Стартовый номер")
     club = models.CharField(verbose_name="Принадлежность к клубу",
                             max_length=50,
-                            blank=True, null=TRUE,)
+                            blank=True, null=True,)
     town = models.CharField(verbose_name="Из какого города?",
                             max_length=50,
-                            blank=True, null=TRUE,)
+                            blank=True, null=True,)
 
     class Meta:
-        unique_together = ('race', 'name', 'surname', )
+        unique_together = ('race', 'name', 'surname', 'patronymic')
 
     def clean(self):
         verges = (self.category.number_start, self.category.number_end)
         num_amount = self.race.numbers_amount
-
-        def get_free_ranges(booked, num_amount, start=1):
-            booked.sort(key=lambda x: x[0])
-            free_nums = []
-            for elem in booked:
-                if start < elem[0]:
-                    free_nums.append((start, elem[0]-1))
-                    start = elem[1] + 1
-            last_booked = booked[-1][1]
-            if last_booked < num_amount:
-                free_nums.append((last_booked + 1, num_amount))
-            return free_nums
-
-        def range_free_nums(free_ranges, booked_nums):
-            booked_nums.sort()
-            nums_amount = len(booked_nums)
-            point = 0
-            free_nums = []
-            for verges in free_ranges:
-                left_verge = verges[0]
-                right_verge = verges[1]
-                for i in range(point, nums_amount):
-                    counter = i
-                    if booked_nums[i] < left_verge:
-                        continue
-                    elif booked_nums[i] == left_verge:
-                        left_verge += 1
-                        continue
-                    elif booked_nums[i] > right_verge:
-                        break
-                    free_nums.append((left_verge, booked_nums[i] - 1),)
-                    left_verge = booked_nums[i] + 1
-                point = counter
-                if left_verge <= right_verge:
-                    free_nums.append((left_verge, right_verge),)
-            return free_nums
-
-        def str_free(free_nums):
-            if not free_nums:
-                return 'Все стартовые номера заняты.'
-            info = 'Для выбора доступны номера:'
-            for verges in free_nums:
-                info += f' {verges[0]}'
-                if verges[0] != verges[1]:
-                    info += f'-{verges[1]}'
-                info += ','
-            return info
-
-        def chek_free(num, free_nums):
-            for verges in free_nums:
-                if num in range(verges[0], verges[1] + 1):
-                    return True
-            return False
-
         nums = list(self.race.race_participants.values_list(
                     'number', flat=True)
                     )
@@ -171,16 +119,17 @@ class Participants(models.Model):
                 ).values_list('number_start', 'number_end')
             )
             if booked:
-                free_ranges = get_free_ranges(booked, num_amount)
-                free_nums = range_free_nums(free_ranges, nums)
+                free_ranges = NumbersChecker.get_free_ranges(
+                    booked, num_amount)
+                free_nums = NumbersChecker.range_free_nums(free_ranges, nums)
             else:
                 free_nums = ((START_NUM, num_amount),)
         else:
-            free_nums = range_free_nums((verges,), nums)
+            free_nums = NumbersChecker.range_free_nums((verges,), nums)
 
-        if not chek_free(self.number, free_nums):
+        if not NumbersChecker.chek_free(self.number, free_nums):
             raise ValidationError(
                 f"номер {self.number} не доступен для "
                 f"категории {self.category}. "
-                f"{str_free(free_nums)}"
+                f"{NumbersChecker.str_free(free_nums)}"
             )
---
+++ ReadyToGo/registration/utilities.py
@@ -0,0 +1,65 @@
+class NumbersChecker():
+    # принимает список занятых диапазонов вида [(1,10), (31,31), (33, 50)],
+    # общее количество номеров (например, 100) и значение начального номера
+    # возвращает список своводных диапазонов в виде
+    #  [(11,30), (32,32), (51, 100)]
+    def get_free_ranges(booked, num_amount, start=1):
+        booked.sort(key=lambda x: x[0])
+        free_nums = []
+        for elem in booked:
+            if start < elem[0]:
+                free_nums.append((start, elem[0]-1))
+                start = elem[1] + 1
+        last_booked = booked[-1][1]
+        if last_booked < num_amount:
+            free_nums.append((last_booked + 1, num_amount))
+        return free_nums
+
+    # принимает список свободных диапазонов вида [(11,30), (32,32), (51, 100)]
+    # и список зарезервированных номеров (1,11,32,33,55)
+    # возвращает список с диапазонами свобоных номеров вида
+    # [(12,30), (51,54), (56, 100)]
+    def range_free_nums(free_ranges, booked_nums):
+        booked_nums.sort()
+        nums_amount = len(booked_nums)
+        point = 0
+        free_nums = []
+        for verges in free_ranges:
+            left_verge = verges[0]
+            right_verge = verges[1]
+            for i in range(point, nums_amount):
+                counter = i
+                if booked_nums[i] < left_verge:
+                    continue
+                elif booked_nums[i] == left_verge:
+                    left_verge += 1
+                    continue
+                elif booked_nums[i] > right_verge:
+                    break
+                free_nums.append((left_verge, booked_nums[i] - 1),)
+                left_verge = booked_nums[i] + 1
+            point = counter
+            if left_verge <= right_verge:
+                free_nums.append((left_verge, right_verge),)
+        return free_nums
+
+    # форматирует список диапазонов вида [(11,30), (32,32), (51, 100)]
+    # в строку вида '11-30, 32, 51-100'
+    def str_free(free_nums):
+        if not free_nums:
+            return 'Все стартовые номера заняты.'
+        info = 'Для выбора доступны номера:'
+        for verges in free_nums:
+            info += f' {verges[0]}'
+            if verges[0] != verges[1]:
+                info += f'-{verges[1]}'
+            info += ','
+        return info
+
+    # проверяет наличие номера в списке диапазонов вида
+    # [(11,30), (32,32), (51, 100)]
+    def chek_free(num, free_nums):
+        for verges in free_nums:
+            if num in range(verges[0], verges[1] + 1):
+                return True
+        return False
