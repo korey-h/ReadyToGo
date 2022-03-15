0542f73f96c5c5ed13c7dd39e1ff13726d1c53af
Егор Уфаев <eufaev@yandex.ru>
2022-03-15 22:08:35 +0500

    gitignore

--- Start_Point/свободные номера.py
+++
@@ -1,9 +0,0 @@
-verges = (40, 60)
-booked = (40, 52, 53, 60)
-
-
-def get_free_nums(verges, booked):
-    return [x for x in range(verges[0], verges[1]+1) if x not in booked]
-
-
-print(get_free_nums(verges, booked))
