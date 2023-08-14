b015663a643bad6065381c94bbb8eb1a423a1418
Егор Уфаев <eufaev@yandex.ru>
2023-08-14 23:04:15 +0500

    Prepare for deploy w docker

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.8.5
+RUN  apt update
+RUN  apt install nano
+RUN mkdir /code
+WORKDIR /code
+COPY ./requirements.txt .
+COPY ./ReadyToGo .
+RUN pip3 install -r requirements.txt --no-cache-dir
+CMD gunicorn StartLine.wsgi:application --bind 0.0.0.0:8001
--- ReadyToGo/StartLine/settings.py
+++ ReadyToGo/StartLine/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'backendrtg']
 
 
 # Application definition
